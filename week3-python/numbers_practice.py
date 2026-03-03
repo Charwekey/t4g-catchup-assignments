0ac0efc258f766e496fb82b094e22b69f78cbf10
Charwekey <crabisabutey@gmail.com>
2026-03-03 10:24:40 +0000

    part3, task2: performed addition, subtraction,  division, floor division, modulo, exponentiation  and multiplication on integers

--- week3-python/numbers_practice.py
+++ week3-python/numbers_practice.py
@@ -3,3 +3,34 @@ num1 = 10
 num2 = 9
 numfloat = 3.14
 numfloat2 = 2.7
+
+
+
+#adds two numbers together
+num_sum = num1 + num2
+print(num_sum)
+
+#subtracts num2 from num1
+num_diff = num1 - num2
+print(num_diff)
+
+#multiplies num1 and num2 together
+num_product = num1 * num2
+print(num_product)
+
+#divides num1 by num2
+num_division = num1 / num2
+print(num_division)
+
+#performs floor division on num1 and num2
+num_floor_division = num1 // num2
+print(num_floor_division)
+
+#calculates the modulus of num1 and num2
+num_modulus = num1 % num2
+print(num_modulus)
+
+#raises num1 to the power of num2
+num_exponentiation = num1 ** num2
+print(num_exponentiation)
+
