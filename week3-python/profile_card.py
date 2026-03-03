2550bbf572c824f74e0fc247c1e4a0c8e48aa2ee
Charwekey <crabisabutey@gmail.com>
2026-03-03 11:16:34 +0000

    part4:declared variables for mini profile card

---
+++ week3-python/profile_card.py
@@ -0,0 +1,14 @@
+
+
+firstname = 'Charwekey'
+lastname = 'Sabutey'
+full_name = (f"{firstname} {lastname}")
+age = 20
+current_week = 4
+cohort = 4
+fav_topic = 'loops'
+cohort_class = 'Backend Class'
+skills_good = "Frontend development"
+
+
+
